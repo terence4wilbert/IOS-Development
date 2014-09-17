1e275f3cc3be7be1012a62abc17e509da9fce896
terence4wilbert <Tall@MacBook.local>
2014-09-17 14:30:14 -0500

    Apps

---
+++ Calculator/Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Terence Wilbert on 9/12/14.
+//  Copyright (c) 2014 Terence Wilbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Terence Wilbert on 9/12/14.
+//  Copyright (c) 2014 Terence Wilbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
